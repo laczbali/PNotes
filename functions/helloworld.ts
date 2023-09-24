7387e90ef2fc989b6a136b37f9343ad70dc7783a
LaczkóBalázs <lab@frankly.dk>
2023-09-24 18:07:08 +0200

    editing dev scripts

--- functions/helloworld.ts
+++ functions/helloworld.ts
@@ -2,7 +2,7 @@ import { Log } from "./Data/Log";
 import { Env } from "./Env";
 
 export const onRequest: PagesFunction<Env> = async (context) => {
-    var x = await (context.env.DB_CONN.prepare("SELECT 1 as test").all());
+    var x = await (context.env.DB_CONN.prepare("SELECT * FROM Logs").all());
     console.log(JSON.stringify(x.results));
 
     return new Response(`hello`);
