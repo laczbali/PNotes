e5b56a206dc86bafc82cae33bcc8744159fd9945
Balázs Laczkó <laczbali@gmail.com>
2023-09-25 21:47:04 +0200

    db schema cleanup

--- functions/Database/Models/Logs.ts
+++ functions/Database/Models/Logs.ts
@@ -1,6 +1,19 @@
+import { sql } from "drizzle-orm";
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
-export const logs = sqliteTable("logs", {
-    message: text('message'),
-    createdAt: integer('createdAt')
+export const logs = sqliteTable("Logs", {
+
+    createdAt: text('CreatedAt')
+        .notNull()
+        .default(sql`current_timestamp`),
+
+    level: text('Level')
+        .$type<'debug' | 'info' | 'warning' | 'error'>()
+        .notNull(),
+
+    message: text('Message')
+        .notNull(),
+
+    error: text('Error')
+
 })
