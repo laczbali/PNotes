2c62c338dc400640f063a93b001573e6f58e0515
LaczkóBalázs <lab@frankly.dk>
2023-10-01 14:15:20 +0200

    dark theme config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SettingsService } from './services/settings.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  public useDarkTheme: boolean = false;
+
+  constructor(private settings: SettingsService) { }
+
+  public get useDarkTheme(): boolean { return this.settings.useDarkTheme; }
+
 }
